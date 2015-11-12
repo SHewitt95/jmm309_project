82bd184fa31eb7966c9b469ec510bc63cf3b9289
Sherman Hewitt <shewitt7@umiami.edu>
2015-11-11 22:27:53 -0500

    Added axis to bar chart.

--- js/bar_chart.js
+++ js/bar_chart.js
@@ -1,26 +1,37 @@
 // Generates a D3 bar chart from the Dept_Avg_Pay.csv file.
 $(document).ready(function() {
 
+  var height = 400;
+  var width = 400;
+
+  var widthScale = d3.scale.linear()
+    .domain([0, 12])
+    .range([0, width]);
+
   d3.csv("../data/Dept_Avg_Pay.csv", function(data) {
+
+      var axis = d3.svg.axis()
+        .scale(widthScale);
 
       var chart = d3.select(".bar_chart").append("svg")
         .attr("width", 100 + "%")
-        .attr("height", 375 + "px") // NOT A GOOD SOLUTION! Get divs to properly sit on top of each other.
-        .attr("overflow", "visible")
+        .attr("height", 400 + "px")
         .attr("display", "block")
-        .attr("clear", "both");
-        //.attr("height", 1000 + "px");
+        .append("g")
+        .attr("transform", "translate(20, 10)")
+        //.call(axis);
 
       chart.selectAll("rect")
         .data(data)
         .enter()
           .append("rect")
-          .attr("width", function(d) {return (d.Pay/1.25) * 25;})
+          .attr("width", function(d) {return d.Pay * 30;})
           .attr("height", 30)
           .attr("y", function(d, i) {return i * 50;})
-          .attr("fill", "blue")
-          .text(function(d) {return d.Pay})
+          .attr("fill", "blue");
 
+      chart.append("g")
+        .attr("transform", "translate(0, 350)")
+        .call(axis);
   })
-
 })
