93f3a617113f7f25084e9083576737ee4d1cf2ff
Sherman Hewitt <shewitt7@umiami.edu>
2015-11-12 18:44:47 -0500

    Progress from JMM 309

--- js/bar_chart.js
+++ js/bar_chart.js
@@ -4,34 +4,35 @@ $(document).ready(function() {
   var height = 400;
   var width = 400;
 
-  var widthScale = d3.scale.linear()
-    .domain([0, 12])
-    .range([0, width]);
-
   d3.csv("../data/Dept_Avg_Pay.csv", function(data) {
 
-      var axis = d3.svg.axis()
-        .scale(widthScale);
+    var widthScale = d3.scale.linear()
+      .domain([0, 12])
+      .range([0, width]);
 
-      var chart = d3.select(".bar_chart").append("svg")
-        .attr("width", 100 + "%")
-        .attr("height", 400 + "px")
-        .attr("display", "block")
-        .append("g")
+    var axis = d3.svg.axis()
+      .scale(widthScale);
+
+    var chart = d3.select(".bar_chart").append("svg")
+      .attr("width", 100 + "%")
+      .attr("height", 400 + "px")
+      .attr("display", "block")
+      .append("g")
         .attr("transform", "translate(20, 10)")
-        //.call(axis);
+        //.a
+      //.call(axis);
 
-      chart.selectAll("rect")
-        .data(data)
-        .enter()
-          .append("rect")
-          .attr("width", function(d) {return d.Pay * 30;})
-          .attr("height", 30)
-          .attr("y", function(d, i) {return i * 50;})
-          .attr("fill", "blue");
+    chart.selectAll("rect")
+      .data(data)
+      .enter()
+        .append("rect")
+        .attr("width", function(d) {return d.Pay * 30;})
+        .attr("height", 30)
+        .attr("y", function(d, i) {return i * 50;})
+        .attr("fill", "blue");
 
-      chart.append("g")
-        .attr("transform", "translate(0, 350)")
-        .call(axis);
+    chart.append("g")
+      .attr("transform", "translate(0, 350)")
+      .call(axis);
   })
 })
