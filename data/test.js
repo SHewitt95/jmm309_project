35d5f9e64e018f175e635ae16b1188c621c12f6a
Sherman Hewitt <shewitt7@umiami.edu>
2015-11-16 16:50:06 -0500

    Switched from test to AllJobs2

---
+++ data/test.js
@@ -0,0 +1,1 @@
+window.DB = {"skills": {"mySkill1":"A", "mySkill2":"B", "mySkill3":"C"}};
--- js/database.js
+++ js/database.js
@@ -9,18 +9,23 @@ $(document).ready(function() {
 
   //console.log(DB[0].Job_Title);
 
+
   var jobTitles = [];
   for (var i = 0; i < DB.length; i++) {
     jobTitles.push(DB[i].Job_Title);
   }
+  
 
-  console.log(jobTitles);
+
+  //console.log(jobTitles);
+  //console.log(DB);
 
   var app = new Ractive({
     el: "#database-rows",
     template: "#row-template",
     data: {
       myJobs: jobTitles
+      //skills: _.keys(DB.skills)
     }
 
   });
