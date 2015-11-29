dda79cf9096ae2e9a254e5323143d30a492d4f00
Sherman Hewitt <shewitt7@umiami.edu>
2015-11-29 16:24:22 -0500

    A lot of stuff. Restyled table, implemented sorting

--- js/database.js
+++ js/database.js
@@ -1,33 +1,29 @@
 $(document).ready(function() {
 
-  //var myDB = $("#database");
-  /*
-  for (var i = 0; i < DB.length; i++) {
-    myDB.append( document.createTextNode(DB[i].Job_Title) );
-  }
-  */
-
-  //console.log(DB[0].Job_Title);
-
-/*
-  var jobTitles = [];
-  for (var i = 0; i < DB.length; i++) {
-    jobTitles.push(DB[i].Job_Title);
-  }
-*/
-
-
-  //console.log(jobTitles);
-  //console.log(DB);
-
   var app = new Ractive({
     el: ".database-rows",
     template: "#row-template",
     data: {
       myJobs: DB,
+      // Code from Ractive.js tutorial: http://learn.ractivejs.org/list-sections/5
+      sort: function ( array, sortColumn ) {
+        array = array.slice();
+        console.log(sortColumn);
 
+        return array.sort( function ( a, b ) {
+          if (sortColumn == "Min_Pay" || sortColumn == "Min_Hours") {
+            // Sorts from least to greatest.
+            return a[ sortColumn ] < b[ sortColumn ] ? -1 : 1;
+          }
+          // Sorts from greatest to least.
+          return a[ sortColumn ] < b[ sortColumn ] ? 1 : -1;
+        });
+      }
     }
+  });
 
+  app.on( 'sort', function ( event, column ) {
+    this.set( 'myColumn', column );
   });
 
 })
