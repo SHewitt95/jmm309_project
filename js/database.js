e302e6a54818e464c71940fde7f2c775ee1d7df9
Sherman Hewitt <shewitt7@umiami.edu>
2015-12-02 17:55:39 -0500

    Toyed with dept slider placement, added DB button

--- js/database.js
+++ js/database.js
@@ -5,11 +5,10 @@ $(document).ready(function() {
     template: "#row-template",
     data: {
       myJobs: DB,
-      
+
       // Code from Ractive.js tutorial: http://learn.ractivejs.org/list-sections/5
       sort: function ( array, sortColumn ) {
         array = array.slice();
-        //console.log(sortColumn);
         return array.sort( function ( a, b ) {
           if (sortColumn == "Min_Pay" || sortColumn == "Min_Hours") {
             // Sorts from least to greatest.
@@ -25,5 +24,15 @@ $(document).ready(function() {
   app.on( 'sort', function ( event, column ) {
     this.set( 'myColumn', column );
   });
+
+
+  function showDB() {
+    $(".database-rows").removeClass("hide");
+  }
+
+  var myButton = document.getElementById("button");
+
+  //$(".database-rows").addClass("hide");
+  myButton.addEventListener("click", showDB, false);
 
 })
